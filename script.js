82c3b6d87f4adb9384c516bba45e0345be371e37
prestigetransportch <prestigetransportch@gmail.com>
2025-11-30 07:18:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -163,20 +163,20 @@ function switchLang(lang) {
 }
 
 const galleryImages = [
-    'imgs/gallery/IMG_0957.png',
-    'imgs/gallery/img_7.jpg',
-    'imgs/gallery/IMG_1188.png',
-    'imgs/gallery/img_1.jpg',
-    'imgs/gallery/img_2.jpg.jpg',
-    'imgs/gallery/img_3.jpg.jpg',
-    'imgs/gallery/img_4.jpg.jpg',
-    'imgs/gallery/img_9.jpg',
-    'imgs/gallery/img_5.jpg.jpg',
-    'imgs/gallery/img_8.jpg',
-    'imgs/gallery/img_6.jpg.jpg',
-    'imgs/gallery/IMG_1433.png',
-    'imgs/gallery/IMG_1510.png',
-    'imgs/gallery/IMG_1513.png'
+    '/imgs/gallery/IMG_0957.png',
+    '/imgs/gallery/img_7.jpg',
+    '/imgs/gallery/IMG_1188.png',
+    '/imgs/gallery/img_1.jpg',
+    '/imgs/gallery/img_2.jpg.jpg',
+    '/imgs/gallery/img_3.jpg.jpg',
+    '/imgs/gallery/img_4.jpg.jpg',
+    '/imgs/gallery/img_9.jpg',
+    '/imgs/gallery/img_5.jpg.jpg',
+    '/imgs/gallery/img_8.jpg',
+    '/imgs/gallery/img_6.jpg.jpg',
+    '/imgs/gallery/IMG_1433.png',
+    '/imgs/gallery/IMG_1510.png',
+    '/imgs/gallery/IMG_1513.png'
 ];
 
 let slideIndex = 0;
